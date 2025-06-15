936f47ff608dab65868b4133e0d858c1d49cff36
ArbotAI <ramileleuterio8@gmail.com>
2025-06-16 01:10:54 +0800

    Activity 3: Update & Delete

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [StudentsController::class, 'index'])->name('std.index');
 Route::post('/create-student', [StudentsController::class, 'newStudent'])->name('std.create');
+    Route::get('/edit-student/{id}', [StudentsController::class, 'editStudent'])->name('std.edit');
+    Route::post('/update-student/{id}', [StudentsController::class, 'updateStudent'])->name('std.update');
+    Route::get('/delete-student/{id}', [StudentsController::class, 'deleteStudent'])->name('std.delete');
